123f4c3b4e38a700b37ef4b7910894cb1c7edef5
jess6718 <6718@holbertonstudents.com>
2024-01-08 09:43:36 +0100

    feat task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basoc annotated function"""
+import math
+
+
+def to_str(n: float) -> str:
+    """To_string function return string representation"""
+    result = str(n)
+    return result
