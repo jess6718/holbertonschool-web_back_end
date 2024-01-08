35cbec376cd4cfc910f55f1102b26cb685875991
jess6718 <6718@holbertonstudents.com>
2024-01-08 09:39:49 +0100

    feat task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basoc annotated function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat function and return"""
+    result = str1 + str2
+    return result
