e89297b902c68f430edbe404e3cf4bd9eda369ba
jess6718 <6718@holbertonstudents.com>
2024-01-09 08:26:27 +0100

    feat task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Asynchronous coroutine wait_n"""
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """List of all delays ascendingly"""
+    result_list = [await task_wait_random(max_delay) for _ in range(n)]
+    for i in range(len(result_list)):
+        swapped = False
+        for j in range(len(result_list) - i - 1):
+            if result_list[j] > result_list[j + 1]:
+                result_list[j], result_list[j + 1] =\
+                        result_list[j + 1], result_list[j]
+                swapped = True
+
+        if not swapped:
+            break  # If no swaps occur, list is already sorted, can break
+    return result_list
