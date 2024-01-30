c99ca3e5c0c3e6cd56ff57eba5dec7c6f996f9cd
jess6718 <6718@holbertonstudents.com>
2024-01-30 09:58:54 +0100

    feat task 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(groceryMap) {
+  if (!(groceryMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [key, value] of groceryMap.entries()) {
+    if (value === 1) {
+      groceryMap.set(key, 100);
+    }
+  }
+  return groceryMap;
+}
