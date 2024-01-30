8eb0915e959d9ab27ef90ca3a8a90c7cec37d803
jess6718 <6718@holbertonstudents.com>
2024-01-30 09:41:59 +0100

    update grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
